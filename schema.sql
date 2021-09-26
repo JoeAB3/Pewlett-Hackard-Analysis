0ba49739cbe2c9cbfb3a67c6c06b59b2a644a9b3
JoeAB3 <jantoniotti@yahoo.com>
2021-09-25 20:23:09 -0400

    7.3.1 redo

--- schema.sql
+++ schema.sql
@@ -8,8 +8,9 @@ CREATE TABLE departments (
 
 
 
+-- Creating tables for PH-EmployeeDB
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
+     emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -18,18 +19,16 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 
--- Creating dept_mgr table for PH-EmployeeDB
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
---create salaries table in PH-EmployeeDB
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -39,25 +38,23 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
--- Create the Titles table with keys
-CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no, title, from_date)
-);
-
--- Create the department employees table with keys
 CREATE TABLE dept_emp (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
+);	
 
 SELECT * FROM departments;
